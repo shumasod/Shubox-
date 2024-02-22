21733f660819f2fd857f263d834e68e285254c68
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-02-22 19:40:13 +0900

    Create User.controller.php

---
+++ Account_maneger/User.controller.php
@@ -0,0 +1,16 @@
+// UserController.php
+
+class UserController extends Controller
+{
+    public function createAccount($userId)
+    {
+        // ユーザーアカウント作成のロジックを実装
+        // 例: Userモデルを使用してユーザーアカウントを作成
+    }
+
+    public function deleteAccount($userId)
+    {
+        // ユーザーアカウント削除のロジックを実装
+        // 例: Userモデルを使用してユーザーアカウントを削除
+    }
+}
