3e38e3f7e9cc73fd13eea58c00379f90103daa21
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-11-12 19:09:41 +0900

    Update and rename Sample.node.js to sample.node.js

--- Backlog/sample.node.js
+++ Backlog/sample.node.js
@@ -4,9 +4,6 @@ const AWS = require('aws-sdk');
 // Initialize AWS Systems Manager Parameter Store
 const ssm = new AWS.SSM();
 
-// Backlog API settings
-const BACKLOG_SPACE = process.env.BACKLOG_SPACE;
-const BACKLOG_BASE_URL = `https://${BACKLOG_SPACE}.backlog.com/api/v2`;
 
 // Function to get API key from Parameter Store
 async function getApiKey() {
